393f1e5544b3d6a937a7484555a3742fee1a3fe7
AlexRivasMachin <arivas028@ikasle.ehu.eus>
2023-10-13 11:55:27 +0200

    WhiteBox

--- src/test/java/CreateForecastDAB.java
+++ src/test/java/CreateForecastDAB.java
@@ -1,8 +1,95 @@
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+
+import dataAccess.DataAccess;
+import dataAccess.DataAccessInterface;
+import domain.Forecast;
+import domain.Question;
+import exceptions.DescriptionDoesntExist;
+import exceptions.ForecastAlreadyExist;
+import exceptions.QuestionDoesntExist;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.persistence.EntityManager;
 
 public class CreateForecastDAB {
+
+    static DataAccess dataAccessMock = Mockito.mock(DataAccess.class);
+    static DataAccessInterface dacMock = Mockito.mock(DataAccessInterface.class);
+    static EntityManager mockEntityManager = Mockito.mock(EntityManager.class);
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testCreateForecastWithNullDescription() throws ForecastAlreadyExist, QuestionDoesntExist, DescriptionDoesntExist {
+        Mockito.when(dataAccessMock.createForecast(null, 1.2F, 1)).thenThrow(new DescriptionDoesntExist());
+        try {
+            dataAccessMock.createForecast(null, 1.2F, 1);
+            fail("Se esperaba excepcion DescriptionDoesntExist");
+        } catch (DescriptionDoesntExist e) {
+
+        }
+    }
+
+    @Test
+    public void testCreateForecastQuestionNumberNull() throws ForecastAlreadyExist, QuestionDoesntExist, DescriptionDoesntExist {
+        /*Mockito.when(dataAccessMock.createForecast("description", 1.2F, null).thenThrow(new QuestionDoesntExist()));
+        try {
+            dataAccessMock.createForecast("description", 1.2F, null);
+            fail("Se esperaba excepcion");
+        } catch (Exception e) {
+
+        }*/
+    }
+   @Test
+    public void testCreateForecastQuestionNumberNotInt() throws ForecastAlreadyExist, QuestionDoesntExist, DescriptionDoesntExist {
+        /*
+        Mockito.when(dataAccessMock.createForecast("description", 1.2F, a).thenThrow(new QuestionDoesntExist()));
+        try {
+            dataAccessMock.createForecast("description", 1.2F, a);
+            fail("Se esperaba excepcion");
+        } catch (Exception e) {
+
+        }
+        */
+    }
+
+    @Test
+    public void testCreateForecastQuestionDoesntExist() throws ForecastAlreadyExist, QuestionDoesntExist, DescriptionDoesntExist {
+        Mockito.when(dataAccessMock.createForecast("description", 1.2F, 0)).thenThrow(new QuestionDoesntExist());
+        try {
+            dataAccessMock.createForecast("description", 1.2F, 0);
+            fail("Se esperaba excepcion QuestionDoesntExist");
+        } catch (QuestionDoesntExist e) {
+
+        }
+    }
+    @Test
+    public void testCreateForecastGainNotFloat() throws ForecastAlreadyExist, QuestionDoesntExist, DescriptionDoesntExist {
+        Mockito.when(dataAccessMock.createForecast("description", -5, 0)).thenThrow(new Exception());
+        try {
+            dataAccessMock.createForecast("description", -5, 0);
+            fail("");
+        } catch (Exception e) {
+
+        }
+    }
+    @Test
+    public void testCreateForecastWithExistingForecast() throws ForecastAlreadyExist, QuestionDoesntExist, DescriptionDoesntExist {
+        Mockito.when(dataAccessMock.createForecast("description", 1.2F, 1)).thenThrow(new ForecastAlreadyExist());
+        try {
+            dataAccessMock.createForecast("description", 1.2F, 1);
+            fail("Se esperaba excepcion ForecastAlreadyExist");
+        } catch (ForecastAlreadyExist e) {
+        }
+
+    }
+
+
+
 
 }
--- src/test/java/CreateForecastDAW.java
+++ src/test/java/CreateForecastDAW.java
@@ -1,39 +1,92 @@
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import dataAccess.DataAccess;
+import dataAccess.DataAccessInterface;
+import domain.Event;
+import domain.Forecast;
 import domain.Question;
+import exceptions.DescriptionDoesntExist;
 import exceptions.ForecastAlreadyExist;
+import exceptions.QuestionAlreadyExist;
 import exceptions.QuestionDoesntExist;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import test.dataAccess.TestDataAccess;
 
 import javax.persistence.EntityManager;
+import java.util.Date;
 
 public class CreateForecastDAW {
 
+
+    static DataAccess dataAccess = new DataAccess();
+    static TestDataAccess testDA = new TestDataAccess();
+
+    Event event;
+    Question ques;
+
     @Before
-    public void setUp() {
+    public void setUp() throws QuestionAlreadyExist {
+        addEvent();
+        dataAccess.open(false);
+    }
+    @After
+        public void tearDown() {
+        removeData();
+        dataAccess.close();
+    }
+
+    public void removeData(){
+        testDA.removeEvent(event);
+    }
+
+    public void addEvent(){
+        try {
+            event = testDA.addEventWithQuestion("Event 1",new Date(), "Question 1", 1);
+        } catch (Exception e) {
+
+        }
+    }
+    @Test
+    public void testCreateForecastWithNullDescription() throws ForecastAlreadyExist, QuestionDoesntExist, DescriptionDoesntExist {
+        try{
+            dataAccess.createForecast(null, 1.2F, 1);
+        }catch (DescriptionDoesntExist e) {
+
+        }
     }
 
     @Test
-    public void testCreateForecastWithNullDescription() throws ForecastAlreadyExist, QuestionDoesntExist {
+    public void testCreateForecastQuestionDoesntExist() throws ForecastAlreadyExist, QuestionDoesntExist, DescriptionDoesntExist {
+        try{
+            dataAccess.createForecast("description", 1.2F, 0);
+        } catch (QuestionDoesntExist e) {
+        }
+    }
+
+    @Test
+    public void testCreateForecastWithExistingForecast() throws ForecastAlreadyExist, QuestionDoesntExist, DescriptionDoesntExist {
+        try {
+            dataAccess.createForecast("description", 1.2F, 1);
+        } catch (ForecastAlreadyExist e) {
+
+        }
 
     }
 
     @Test
-    public void testCreateForecastWithNullQuestion() throws ForecastAlreadyExist, QuestionDoesntExist {
+    public void testCreateForecastSuccess() throws ForecastAlreadyExist, QuestionDoesntExist, DescriptionDoesntExist {
+        try {
+        dataAccess.createForecast("description", 1.2F, 1);
 
+        } catch (ForecastAlreadyExist | QuestionDoesntExist | DescriptionDoesntExist e) {
+
+        }
     }
 
 
-    @Test
-    public void testCreateForecastWithExistingForecast() throws ForecastAlreadyExist, QuestionDoesntExist {
-
-    }
-
-    @Test
-    public void testCreateForecastSuccess() throws ForecastAlreadyExist, QuestionDoesntExist {
-
-    }
 }
-
