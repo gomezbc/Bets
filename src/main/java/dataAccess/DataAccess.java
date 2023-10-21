f3691f7769a5e008c3d56e94d43b0acdee9bc1be
martin <mlopezdeipina001@ikasle.ehu.eus>
2023-10-21 14:37:24 +0200

    refactor2 y 3

--- src/main/java/dataAccess/DataAccess.java
+++ src/main/java/dataAccess/DataAccess.java
@@ -59,100 +59,30 @@ public class DataAccess  implements DataAccessInterface{
 		   int month=today.get(Calendar.MONTH);
 		   month+=1;
 		   int year=today.get(Calendar.YEAR);
-		   if (month==12) { month=0; year+=1;}  
-	    
-			Event ev1=new Event(1, "Atletico-Athletic", UtilDate.newDate(year,month,17));
-			Event ev2=new Event(2, "Eibar-Barcelona", UtilDate.newDate(year,month,17));
-			Event ev3=new Event(3, "Getafe-Celta De Vigo", UtilDate.newDate(year,month,17));
-			Event ev4=new Event(4, "Alaves-Deportivo", UtilDate.newDate(year,month,17));
-			Event ev5=new Event(5, "Espanyol-Villareal", UtilDate.newDate(year,month,17));
-			Event ev6=new Event(6, "Las Palmas-Sevilla", UtilDate.newDate(year,month,17));
-			Event ev7=new Event(7, "Malaga-Valencia", UtilDate.newDate(year,month,17));
-			Event ev8=new Event(8, "Girona-Leganés", UtilDate.newDate(year,month,17));
-			Event ev9=new Event(9, "Real Sociedad-Levante", UtilDate.newDate(year,month,17));
-			Event ev10=new Event(10, "Betis-Real Madrid", UtilDate.newDate(year,month,17));
+		   if (month==12) { month=0; year+=1;}
 
-			Event ev11=new Event(11, "Atletico-Athletic", UtilDate.newDate(year,month,1));
-			Event ev12=new Event(12, "Eibar-Barcelona", UtilDate.newDate(year,month,1));
-			Event ev13=new Event(13, "Getafe-Celta De Vigo", UtilDate.newDate(year,month,1));
-			Event ev14=new Event(14, "Alavés-Deportivo", UtilDate.newDate(year,month,1));
-			Event ev15=new Event(15, "Espanyol-Villareal", UtilDate.newDate(year,month,1));
-			Event ev16=new Event(16, "Las Palmas-Sevilla", UtilDate.newDate(year,month,1));
-			
+			List<String> eventNames = new ArrayList(Arrays.asList("Atletico-Athletic","Eibar-Barcelona","Getafe-Celta","Alaves-Deportivo","Espanyol-Villareal",
+					"Las Palmas-Sevilla","Malaga-Valencia","Girona-Leganés","Real Sociedad-Levante","Betis-Real Madrid", "Atletico-Athletic", "Eibar-Barcelona",
+					"Getafe-Celta", "Alaves-Deportivo", "Espanyol-Villareal", "Las Palmas-Sevilla", "Málaga-Valencia", "Girona-Leganés", "Real Sociedad-Levante", "Betis-Real Madrid"));
 
-			Event ev17=new Event(17, "Málaga-Valencia", UtilDate.newDate(year,month+1,28));
-			Event ev18=new Event(18, "Girona-Leganés", UtilDate.newDate(year,month+1,28));
-			Event ev19=new Event(19, "Real Sociedad-Levante", UtilDate.newDate(year,month+1,28));
-			Event ev20=new Event(20, "Betis-Real Madrid", UtilDate.newDate(year,month+1,28));
-			
-			Question q1;
-			Question q2;
-			Question q3;
-			Question q4;
-			Question q5;
-			Question q6;
+			List<Event> events = createEvents(eventNames, year, month);
+
+			List<String>[] questionNames = new ArrayList[]{
+					new ArrayList(Arrays.asList("¿Quién ganará el partido?", "¿Quién meterá el primer gol?", "¿Cuántos goles se marcarán?", "¿Habrá goles en la primera parte?")),
+					new ArrayList(Arrays.asList("Who will win the match?", "Who will score first?", "How many goals will be scored in the match?", "Will there be goals in the first half?")),
+					new ArrayList(Arrays.asList("Zeinek irabaziko du partidua?", "Zeinek sartuko du lehenengo gola?", "Zenbat gol sartuko dira?", "Golak sartuko dira lehenengo zatian?"))
+			};
+
+			List<Question> questions = createQuestionsByLanguaje(questionNames, events);
 					
-			if (Locale.getDefault().equals(new Locale("es"))) {
-				q1=ev1.addQuestion("¿Quién ganará el partido?",1);
-				q2=ev1.addQuestion("¿Quién meterá el primer gol?",2);
-				q3=ev11.addQuestion("¿Quién ganará el partido?",1);
-				q4=ev11.addQuestion("¿Cuántos goles se marcarán?",2);
-				q5=ev17.addQuestion("¿Quién ganará el partido?",1);
-				q6=ev17.addQuestion("¿Habrá goles en la primera parte?",2);
-			}
-			else if (Locale.getDefault().equals(new Locale("en"))) {
-				q1=ev1.addQuestion("Who will win the match?",1);
-				q2=ev1.addQuestion("Who will score first?",2);
-				q3=ev11.addQuestion("Who will win the match?",1);
-				q4=ev11.addQuestion("How many goals will be scored in the match?",2);
-				q5=ev17.addQuestion("Who will win the match?",1);
-				q6=ev17.addQuestion("Will there be goals in the first half?",2);
-			}			
-			else {
-				q1=ev1.addQuestion("Zeinek irabaziko du partidua?",1);
-				q2=ev1.addQuestion("Zeinek sartuko du lehenengo gola?",2);
-				q3=ev11.addQuestion("Zeinek irabaziko du partidua?",1);
-				q4=ev11.addQuestion("Zenbat gol sartuko dira?",2);
-				q5=ev17.addQuestion("Zeinek irabaziko du partidua?",1);
-				q6=ev17.addQuestion("Golak sartuko dira lehenengo zatian?",2);
-				
-			}
-			
-			q1.addForecast("Athletic", 1.5f ,q1);
-			q1.addForecast("Atlético", 1.4f ,q1);
-			q2.addForecast("Athletic", 1.8f ,q2);
-			q2.addForecast("Atlético", 1.2f ,q2);
-			
-			db.persist(q1);
-			db.persist(q2);
-			db.persist(q3);
-			db.persist(q4);
-			db.persist(q5);
-			db.persist(q6); 
-	
-	        
-			db.persist(ev1);
-			db.persist(ev2);
-			db.persist(ev3);
-			db.persist(ev4);
-			db.persist(ev5);
-			db.persist(ev6);
-			db.persist(ev7);
-			db.persist(ev8);
-			db.persist(ev9);
-			db.persist(ev10);
-			db.persist(ev11);
-			db.persist(ev12);
-			db.persist(ev13);
-			db.persist(ev14);
-			db.persist(ev15);
-			db.persist(ev16);
-			db.persist(ev17);
-			db.persist(ev18);
-			db.persist(ev19);
-			db.persist(ev20);	
-			
-			
+			questions.get(0).addForecast("Athletic", 1.5f ,questions.get(0));
+			questions.get(0).addForecast("Atlético", 1.4f ,questions.get(0));
+			questions.get(1).addForecast("Athletic", 1.8f ,questions.get(1));
+			questions.get(1).addForecast("Atlético", 1.2f ,questions.get(1));
+
+			persistQuestions(questions);
+			persistEvents(events);
+
 			db.getTransaction().commit();
 			System.out.println("Db initialized");
 		}
@@ -160,9 +90,78 @@ public class DataAccess  implements DataAccessInterface{
 			e.printStackTrace();
 		}
 	}
-	
-	
-	
+
+	private List<Event> createEvents(List<String> eventNames, int year, int month) {
+		List<Event> events = new ArrayList<>();
+
+		for(int i = 1; i < eventNames.size(); i++){
+			events.add(getIndividualEvent(eventNames, year, month, i));
+		}
+
+		return events;
+	}
+
+	private Event getIndividualEvent(List<String> eventNames, int year, int month, int i) {
+		Event event = null;
+		if(i <= 10){
+			event = new Event(i, eventNames.get(i), UtilDate.newDate(year, month, 17));
+		}
+		if(i >= 11 && i <= 16){
+			event = new Event(i, eventNames.get(i), UtilDate.newDate(year, month, 1));
+		}
+		if(i >= 17 && i <= 20){
+			event = new Event(i, eventNames.get(i), UtilDate.newDate(year, month +1, 28));
+		}
+		return event;
+	}
+
+	private List<Question> createQuestionsByLanguaje(List<String>[] questionNames, List<Event> events) {
+		switch (Locale.getDefault().toString()){
+			case "es_ES":
+				return createQuestion(questionNames[0], events);
+			case "en_US":
+				return createQuestion(questionNames[1], events);
+			case "eu_ES":
+				return createQuestion(questionNames[2], events);
+			default:
+				return createQuestion(questionNames[0], events);
+		}
+	}
+
+	private List<Question> createQuestion(List<String> questionNames, List<Event> events){
+		List<Question> questions = new ArrayList<>();
+
+		questions.add(events.get(0).addQuestion(questionNames.get(0), 1));
+		questions.add(events.get(0).addQuestion(questionNames.get(1), 2));
+		questions.add(events.get(10).addQuestion(questionNames.get(0), 1));
+		questions.add(events.get(10).addQuestion(questionNames.get(2), 2));
+		questions.add(events.get(16).addQuestion(questionNames.get(0), 1));
+		questions.add(events.get(16).addQuestion(questionNames.get(3), 2));
+
+		return questions;
+	}
+
+	private static void persistQuestions(List<Question> questions) {
+		for(Question q: questions){
+			db.persist(q);
+		}
+	}
+
+	private static void persistEvents(List<Event> events) {
+		for(Event ev: events){
+			db.persist(ev);
+		}
+	}
+
+
+
+
+
+
+
+
+
+
 	/**
 	 * This method creates a question for an event, with a question text and the minimum bet
 	 * 
@@ -417,6 +416,7 @@ public class DataAccess  implements DataAccessInterface{
 		checkIfUserAndBetInDB(user, bet);
 
 		modifyBetIfPossible(betMoney, bet, user);
+
 		return bet;
 	}
 	private void checkIfDNIAndBetNumberRight(int betNumber, String dni) throws UserDoesntExist, BetDoesntExist {
@@ -439,20 +439,18 @@ public class DataAccess  implements DataAccessInterface{
 		checkIfUserInDB(user);
 		checkIfBetInDB(bet);
 	}
-
-	private static void checkIfUserInDB(User user) throws UserDoesntExist {
+	private void checkIfUserInDB(User user) throws UserDoesntExist {
 		if (user == null) {
 			System.err.println(">> DataAccess: modifyBet => error UserDoesntExist: No existe un usuario con este DNI en la base de datos");
 			throw new UserDoesntExist("No existe un usuario con este DNI en la base de datos");
 		}
 	}
-	private static void checkIfBetInDB(Bet bet) throws BetDoesntExist {
+	private void checkIfBetInDB(Bet bet) throws BetDoesntExist {
 		if (bet == null) {
 			System.err.println(">> DataAccess: modifyBet => error BetDoesntExist: No existe la apuesta ha modificar");
 			throw new BetDoesntExist("No existe la apuesta a modificar");
 		}
 	}
-
 	private void modifyBetIfPossible(float betMoney, Bet bet, User user) {
 		double betMoneyBefore = bet.getBetMoney();
 		double betMoneyAfter = betMoney + betMoneyBefore;
@@ -493,10 +491,10 @@ public class DataAccess  implements DataAccessInterface{
 		user2.setName(nombre);
 		db.persist(user2);
 		db.getTransaction().commit();
-		
-		
+
+
 	}
-	
+
 	/**
 	 * Este método nos permite cambiar el apellido de un usuario
 	 * @param user, el usuario al que se le va a cambiar el apellido
@@ -592,6 +590,8 @@ public class DataAccess  implements DataAccessInterface{
 		db.getTransaction().commit();
 		return user2;
 	}
+
+
 
 
 	/**
